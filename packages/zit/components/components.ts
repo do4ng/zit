54365fb5c533aee24650d890e41154a77fd2e006
r.ehdals <57896501+do4ng@users.noreply.github.com>
2022-11-09 22:30:20 +0900

    feat: add fragment component

---
+++ packages/zit/components/components.ts
@@ -0,0 +1,3 @@
+import fragment from './fragment';
+
+export { fragment, fragment as container };
---
+++ packages/zit/components/fragment.ts
@@ -0,0 +1,3 @@
+import { createElement } from '../src';
+
+export default createElement(null, '{{slug}}', { slug: '' });
