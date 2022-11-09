701ae17b51bff48138a98dba7f35277a1ce5ebcb
r.ehdals <57896501+do4ng@users.noreply.github.com>
2022-11-09 22:28:38 +0900

    example: add Router

--- example/index.js
+++ example/index.js
@@ -1,38 +1,63 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
-import * as zite from '../packages/zit/dist';
+import * as zit from '../packages/zit/dist';
 
-console.log(zite);
-
-const [count, setCount] = zite.useStore(0);
+const [count, setCount] = zit.useStore(0);
 
 const app = document.getElementById('app');
-const introduce = zite.createElement('h1', '<h1>Greeting</h1>');
-const greet = zite.createElement('div', '<div>hello, {{name}}</div>', { name: 'world' });
-const container = zite.createElement('div', '<div>{{slug}}</div>');
+const introduce = zit.createElement('h1', '<h1>Greeting</h1>');
+const greet = zit.createElement('div', '<div>hello, {{name}}</div>', { name: 'world' });
+const container = zit.createElement('div', '<div>{{slug}}</div>');
 
-const result = zite.html`${introduce}
-  <p>name: world</p>
-  ${container(greet)}
-  <p>name: cat</p>
-  ${greet({ name: 'cat' })}
-  
-  <button id="up">+</button>
-  <button id="down">-</button>
+const result = zit.createElement(
+  null,
+  zit.html`${introduce}
+    <p>name: world</p>
+    ${container(greet)}
+    <p>name: cat</p>
+    ${greet({ name: 'cat' })}
 
-  ${count}
-  `;
+    <button id="up">+</button>
+    <button id="down">-</button>
 
-app.innerHTML = result;
+    ${count} `
+);
 
-// counter
+/*
 
-function up() {
-  setCount((pre) => pre + 1);
-}
-function down() {
-  setCount((pre) => pre - 1);
-}
+main
 
-document.getElementById('up').addEventListener('click', up);
-document.getElementById('down').addEventListener('click', down);
+*/
+
+const user = zit.createElement(null, "<strong>{{user}}</strong>'s page");
+
+const routing = {
+  // main
+  '/': {
+    template:
+      // eslint-disable-next-line no-multi-str
+      '<h1>Hello World!</h1>\
+      <a href="/counter">Counter</a> <a href="/users/0">user 0</a> <a href="/users/1">user 1</a>',
+  },
+  // counter
+  '/counter': {
+    template: result,
+    js: () => {
+      function up() {
+        setCount((pre) => pre + 1);
+      }
+      function down() {
+        setCount((pre) => pre - 1);
+      }
+
+      document.getElementById('up').addEventListener('click', up);
+      document.getElementById('down').addEventListener('click', down);
+    },
+  },
+  // users
+  '/users/:user': {
+    template: user,
+  },
+};
+
+zit.useRouter(routing, app);
--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,12 @@ import vite from 'vite';
 
 export default vite.defineConfig({
   base: '.',
+  server: {
+    port: 8080,
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 });
