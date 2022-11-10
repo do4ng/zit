28c96f6946e233afe42a6b020e32ddc5edcd705d
r.ehdals <57896501+do4ng@users.noreply.github.com>
2022-11-10 23:30:04 +0900

    for browser

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,32 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const esbuild = require('esbuild');
 const { nodeExternalsPlugin } = require('esbuild-node-externals');
+const { readFileSync, writeFileSync, existsSync, mkdirSync } = require('fs');
 const { join } = require('path');
 
 const base = {
   platform: '',
 };
 
-const build = (pkg, entryPoint = 'src/index.ts', out = 'dist/node.js', cfg = {}) => {
-  esbuild.build({
-    entryPoints: [join(process.cwd(), 'packages', pkg, entryPoint)],
-    outfile: join(process.cwd(), 'packages', pkg, out),
-    target: 'esnext',
-    plugins: [nodeExternalsPlugin({ packagePath: join(process.cwd(), 'packages', pkg, 'package.json') })],
-    ...base,
-    ...cfg,
-    platform: 'node',
-    bundle: true,
-  });
+const build = (pkg, entryPoint = 'src/browser.ts', out = 'dist/browser.js', cfg = {}) => {
+  esbuild
+    .build({
+      entryPoints: [join(process.cwd(), 'packages', pkg, entryPoint)],
+      outfile: join(process.cwd(), 'packages', pkg, out),
+      target: 'esnext',
+      // plugins: [nodeExternalsPlugin({ packagePath: join(process.cwd(), 'packages', pkg, 'package.json') })],
+      ...base,
+      ...cfg,
+      platform: 'browser',
+      bundle: true,
+    })
+    .then(() => {
+      const file = readFileSync(join(__dirname, '../packages/zit/dist/index.js')).toString();
+
+      if (!existsSync(join(__dirname, '../web/dist'))) mkdirSync(join(__dirname, '../web/dist'));
+
+      writeFileSync(join(__dirname, '../web/dist/zit.js'), file);
+    });
 };
 const browser = (pkg, entryPoint = 'src/index.ts', out = 'dist/index.js', cfg = {}) => {
   esbuild.build({
